2888a86201535f9986ea923931f2829c73ea0d9a
iamcopper <kangpan519@gmail.com>
2021-12-20 17:04:43 +0800

    [hardware] add more control function for watchdog_test.c

--- hardware/watchdot_test.c
+++ hardware/watchdot_test.c
@@ -1,16 +1,50 @@
 /**
  * Base on kernel source code tree:
- *   samples/watchdog/watchdog-simple.c
- * and do some personal changes.
+ * watchdog-simple.c and do some personal changes.
+ *
+ * Kernel Test Demo:
+ * <KERNEL_SRC>/samples/watchdog/watchdog-simple.c
+ *
+ * Kernel Document:
+ * https://www.kernel.org/doc/html/latest/watchdog/wdt.html
+ * https://www.kernel.org/doc/html/latest/watchdog/watchdog-api.html
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <linux/watchdog.h>
 
 #define WATCHDOG     "/dev/watchdog"
 #define INTEVAL_SEC  1
+
+void feed(int fd)
+{
+	ioctl(fd, WDIOC_KEEPALIVE, 0);
+}
+
+void set_timeout(int fd, int timeout)
+{
+	ioctl(fd, WDIOC_SETTIMEOUT, &timeout);
+	printf("The timeout was set to %d seconds\n", timeout);
+}
+
+int get_timeout(int fd)
+{
+	int timeout = 0;
+	ioctl(fd, WDIOC_GETTIMEOUT, &timeout);
+	printf("timeout = %d\n", timeout);
+	return timeout;
+}
+
+int get_timeleft(int fd)
+{
+	int timeleft = 0;
+	ioctl(fd, WDIOC_GETTIMELEFT, &timeleft);
+	printf("timeleft = %d\n", timeleft);
+	return timeleft;
+}
 
 int main(void)
 {
