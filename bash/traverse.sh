348a2830e1a6bc0b0fbffcc1679488a1392c3c37
iamcopper <kangpan519@gmail.com>
2022-07-08 18:55:04 +0800

    [traverse.sh] add function call back in traverse

--- bash/traverse.sh
+++ bash/traverse.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
+function my_func()
+{
+	for (( i = 1; i <= $#; i++ )); do
+		echo "[${FUNCNAME}] --- ${!i}"
+	done
+}
+
 function traverse()
 {
-	local dir=${1}
+	local dir=$1
+	local func=$2
 	for file in $(ls ${dir})
 	do
-		current=${dir}"/"${file}
+		current=${dir}/${file}
 		if [[ -d ${current} ]]; then
-			traverse ${current}
+			${FUNCNAME} ${current} ${func}
 		else
-			echo ${current}
+			${func} ${current}
 		fi
 	done
 }
 
 if [[ $# -lt 1 ]]; then
-	traverse .
+	traverse . my_func
 else
-	traverse $1
+	traverse $1 my_func
 fi
