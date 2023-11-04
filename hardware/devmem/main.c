ea4a5ad24af90162d826c38d5fccf4a02c474d2b
iamcopper <kangpan519@gmail.com>
2023-11-04 23:25:00 +0800

    [hardware/devmem] Format usage() indent.

--- hardware/devmem/main.c
+++ hardware/devmem/main.c
@@ -9,8 +9,8 @@ void usage(char *appname)
 	printf("\nRead/write from physical address\n\n");
 	printf("Usage: %s <address> [<width> [value]]\n\n", appname);
 	printf("  address --- address to read or write\n");
-    printf("  width   --- width 8/16/[32]\n");
-    printf("  value   --- value to be written\n\n");
+	printf("  width   --- width 8/16/[32]\n");
+	printf("  value   --- value to be written\n\n");
 }
 
 int main(int argc, char *argv[])
