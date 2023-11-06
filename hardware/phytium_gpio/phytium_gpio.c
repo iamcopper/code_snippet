1166dcf171fd476973a68e9de19bc2221ce79ef4
iamcopper <kangpan519@gmail.com>
2023-11-06 18:17:45 +0800

    [hardware/phytium_gpio] Change phytium_gpio interface from "gpio_open\gpio_close\... to "gpio_init/...".

--- hardware/phytium_gpio/phytium_gpio.c
+++ hardware/phytium_gpio/phytium_gpio.c
@@ -7,8 +7,39 @@
 #include "phytium_gpio.h"
 #include "devmem.h"
 
+/* Phytium 2004/D2000 */
+#define PINMUX_ADDR   0x28180000
+#define GPIO_0_ADDR   0x28004000
+#define GPIO_1_ADDR   0x28005000
+
 #define ARRAY_SIZE(array) \
 	sizeof(array)/(sizeof(array[0]))
+
+typedef struct gpio {
+	uint8_t ctrl;        /* gpio controller (0, 1) */
+	char    port;        /* gpio port (a, b) */
+	uint8_t pin;         /* gpio pin (0-7) */
+} gpio_t;
+
+typedef struct gpio_pinmux_cfg {
+	gpio_t   gpio;
+	uint64_t addr;       /* gpio pinmux address */
+	uint8_t  bit_offset; /* gpio pinmux offset (2 bits) */
+	uint8_t  val;        /* gpio pinmux value (2 bits) */
+} gpio_pinmux_cfg_t;
+
+typedef struct gpio_addr {
+	gpio_t   gpio;
+	uint64_t dir_addr;   /* gpio direction address */
+	uint64_t in_addr;    /* gpio input address */
+	uint64_t out_addr;   /* gpio output address */
+} gpio_addr_t;
+
+typedef struct gpio_desc {
+	gpio_t   gpio;
+	const gpio_pinmux_cfg_t *pinmux; /* gpio pinmux cfg info */
+	const gpio_addr_t       *addr;   /* gpio addr info */
+} gpio_desc_t;
 
 const static gpio_pinmux_cfg_t gpio_pinmux_cfg[] = {
 	{{0, 'a', 0}, PINMUX_ADDR + 0x200, 25, 0x1},
@@ -52,85 +83,38 @@ const static gpio_addr_t gpio_addr[] = {
 	{{1, 'b', 0}, GPIO_1_ADDR + 0x10, GPIO_1_ADDR + 0x14, GPIO_1_ADDR + 0x0C},
 };
 
-static int _gpio_set_pinmux_reg(const gpio_desc_t *gpio_desc)
+#define GPIO_CMP_ALL       (GPIO_CMP_CTRL | GPIO_CMP_PORT | GPIO_CMP_PIN)
+#define GPIO_CMP_CTRL_PORT (GPIO_CMP_CTRL | GPIO_CMP_PORT)
+#define GPIO_CMP_CTRL      (1 << 2)
+#define GPIO_CMP_PORT      (1 << 1)
+#define GPIO_CMP_PIN       (1 << 0)
+
+static int _gpio_cmp(const gpio_t *gpio1, const gpio_t *gpio2, uint8_t flag)
 {
-	int devmem_fd, ret;
-	uint32_t devmem_val;
-	const gpio_pinmux_cfg_t *pinmux = NULL;
+	if ((flag & GPIO_CMP_CTRL) && (gpio1->ctrl != gpio2->ctrl)) {
+		return (gpio1->ctrl - gpio2->ctrl);
+	}
 
-	if (!gpio_desc || !gpio_desc->pinmux)
-		return -1;
+	if ((flag & GPIO_CMP_PORT) && (gpio1->port != gpio2->port))  {
+		return (gpio1->port - gpio2->port);
+	}
 
-	pinmux = gpio_desc->pinmux;
-	devmem_fd = devmem_open((void *)pinmux->addr);
-	if (devmem_fd <= 0)
-		return -1;
-	ret = devmem_read(devmem_fd, 32, &devmem_val);
-	if (ret < 0)
-		return -1;
-
-	devmem_val &= ~(0x3 << pinmux->bit_offset);
-	devmem_val |= (pinmux->val << pinmux->bit_offset);
-
-	ret = devmem_write(devmem_fd, 32, devmem_val);
-	if (ret < 0)
-		return -1;
-	ret = devmem_close(devmem_fd);
-	if (ret < 0)
-		return -1;
+	if (flag & GPIO_CMP_PIN)  {
+		return (gpio1->pin- gpio2->pin);
+	}
 
 	return 0;
 }
 
-static int _gpio_set_dir_reg(const gpio_desc_t *gpio_desc)
+
+static int _gpio_desc_init(gpio_desc_t *gpio_desc, const char *gpioname)
 {
-	int devmem_fd, ret;
-	uint32_t devmem_val;
-
-	if (!gpio_desc || !gpio_desc->addr)
-		return -1;
-
-	devmem_fd = devmem_open((void *)gpio_desc->addr->dir_addr);
-	if (devmem_fd <= 0)
-		return -1;
-	ret = devmem_read(devmem_fd, 32, &devmem_val);
-	if (ret < 0)
-		return -1;
-
-	devmem_val &= ~(0x1 << gpio_desc->gpio.pin);
-	if (gpio_desc->dir == GPIO_DIR_OUT)
-		devmem_val |= (0x1 << gpio_desc->gpio.pin);
-
-	ret = devmem_write(devmem_fd, 32, devmem_val);
-	if (ret < 0)
-		return -1;
-	ret = devmem_close(devmem_fd);
-	if (ret < 0)
-		return -1;
-
-	return 0;
-}
-
-static gpio_desc_t *_gpio_desc_init(const char *gpioname, uint8_t gpiodir)
-{
-	gpio_desc_t *gpio_desc = NULL;
-	char strtmp[GPIO_NAME_LEN_MAX] = {0};
 	char *str = NULL;
+	char strtmp[GPIO_NAME_LEN_MAX] = {0};
 	int i;
 
-	if (!gpioname)
-		return NULL;
-
-	gpio_desc = malloc(sizeof(gpio_desc_t));
-	if (!gpio_desc) {
-		perror("malloc gpio_desc");
-		return NULL;
-	}
-
-	if (gpiodir != GPIO_DIR_IN && gpiodir != GPIO_DIR_OUT) {
-		fprintf(stderr, "[ERROR] GPIO DIR error");
-	}
-	gpio_desc->dir = gpiodir;
+	if (!gpio_desc || !gpioname)
+		return -1;
 
 	strncpy(strtmp, gpioname, GPIO_NAME_LEN_MAX);
 
@@ -138,129 +122,141 @@ static gpio_desc_t *_gpio_desc_init(const char *gpioname, uint8_t gpiodir)
 	str = strtok(strtmp, "-");
 	if (!str) {
 		fprintf(stderr, "[ERROR] GPIO CTRL get error");
-		goto err;
+		return -1;
 	}
 	gpio_desc->gpio.ctrl = strtoul(str, NULL, 0);
 	if (errno || gpio_desc->gpio.ctrl >= GPIO_CTRL_NUM) {
 		fprintf(stderr, "[ERROR] GPIO CTRL format or range error");
-		goto err;
+		return -1;
 	}
 
 	/* get gpio port */
 	str = strtok(str, "-");
 	if (!str) {
 		fprintf(stderr, "[ERROR] GPIO PORT get error");
-		goto err;
+		return -1;
 	}
 	gpio_desc->gpio.port = tolower(str[0]);
 	if (gpio_desc->gpio.port < 'a' || gpio_desc->gpio.port > 'b') {
 		fprintf(stderr, "[ERROR] GPIO PORT range error");
-		goto err;
+		return -1;
 	}
 
 	/* get gpio pin */
 	str = strtok(str, "-");
 	if (!str) {
 		fprintf(stderr, "[ERROR] GPIO PIN get error");
-		goto err;
+		return -1;
 	}
 	gpio_desc->gpio.pin  = strtoul(str, NULL, 0);
 	if (errno || gpio_desc->gpio.pin >= GPIO_PIN_NUM) {
 		fprintf(stderr, "[ERROR] GPIO PIN format or range error");
-		goto err;
+		return -1;
 	}
 
 	/* get pinmux cfg */
 	for (i = 0; i < ARRAY_SIZE(gpio_pinmux_cfg); i++) {
-		if (gpio_pinmux_cfg[i].gpio.ctrl == gpio_desc->gpio.ctrl
-		 && gpio_pinmux_cfg[i].gpio.port == gpio_desc->gpio.port
-		 && gpio_pinmux_cfg[i].gpio.pin  == gpio_desc->gpio.pin) {
-
+		if (0 == _gpio_cmp(&gpio_desc->gpio, &gpio_pinmux_cfg[i].gpio, GPIO_CMP_ALL)) {
 			gpio_desc->pinmux = &gpio_pinmux_cfg[i];
 			break;
 		}
 	}
 	if (!gpio_desc->pinmux) {
 		fprintf(stderr, "[ERROR] GPIO pinmux_cfg is NULL");
-		goto err;
+		return -1;
 	}
 
 	/* get gpio addr info */
 	for (i = 0; i < ARRAY_SIZE(gpio_addr); i++) {
-		if (gpio_addr[i].gpio.ctrl == gpio_desc->gpio.ctrl
-		 && gpio_addr[i].gpio.port == gpio_desc->gpio.port) {
-
+		if (0 == _gpio_cmp(&gpio_desc->gpio, &gpio_addr[i].gpio, GPIO_CMP_CTRL_PORT)) {
 			gpio_desc->addr = &gpio_addr[i];
 			break;
 		}
 	}
 	if (!gpio_desc->addr) {
 		fprintf(stderr, "[ERROR] GPIO gpio_addr is NULL");
-		goto err;
-	}
-
-	return gpio_desc;
-
-err:
-	if (gpio_desc) {
-		free(gpio_desc);
-		gpio_desc = NULL;
-	}
-	return NULL;
-}
-
-gpio_desc_t *gpio_open(const char *gpioname, uint8_t gpiodir)
-{
-	gpio_desc_t *gpio_desc = NULL;
-	int ret;
-
-	gpio_desc = _gpio_desc_init(gpioname, gpiodir);
-	if (!gpio_desc) {
-		fprintf(stderr, "[ERROR] GPIO Read desc failed.");
-		return NULL;
-	}
-
-	ret = _gpio_set_pinmux_reg(gpio_desc);
-	if (ret < 0) {
-		fprintf(stderr, "[ERROR] GPIO Set pinmux reg failed.");
-		return NULL;
-	}
-
-	ret = _gpio_set_dir_reg(gpio_desc);
-	if (ret < 0) {
-		fprintf(stderr, "[ERROR] GPIO Set dir reg failed.");
-		return NULL;
-	}
-
-	return gpio_desc;
-}
-
-int gpio_close(gpio_desc_t *gpio_desc)
-{
-	if (!gpio_desc)
 		return -1;
+	}
 
-	free(gpio_desc);
 	return 0;
 }
 
-int gpio_set(gpio_desc_t *gpio_desc, uint8_t val)
+static int _gpio_set_pinmux_reg(const gpio_desc_t *gpio_desc)
 {
-	int devmem_fd, ret;
+	int ret;
+	uint32_t devmem_val;
+
+	if (!gpio_desc || !gpio_desc->pinmux)
+		return -1;
+
+	ret = devmem_read2((void *)gpio_desc->pinmux->addr, 32, &devmem_val);
+	if (ret < 0)
+		return -1;
+
+	/* phytium gpio pinmux: 2 bits */
+	devmem_val &= ~(0x3 << gpio_desc->pinmux->bit_offset);
+	devmem_val |= (gpio_desc->pinmux->val << gpio_desc->pinmux->bit_offset);
+
+	ret = devmem_write2((void *)gpio_desc->pinmux->addr, 32, devmem_val);
+	if (ret < 0)
+		return -1;
+
+	return 0;
+}
+
+static int _gpio_set_dir_reg(const gpio_desc_t *gpio_desc, uint8_t dir)
+{
+	int ret;
 	uint32_t devmem_val;
 
 	if (!gpio_desc || !gpio_desc->addr)
 		return -1;
 
-	if (gpio_desc->dir != GPIO_DIR_OUT) {
-		fprintf(stderr, "[ERROR] It's not a OUTPUT PIN.");
+	ret = devmem_read2((void *)gpio_desc->addr->dir_addr, 32, &devmem_val);
+	if (ret < 0)
 		return -1;
-	}
 
-	devmem_fd = devmem_open((void *)gpio_desc->addr->out_addr);
-	if (devmem_fd <= 0)
+	devmem_val &= ~(0x1 << gpio_desc->gpio.pin);
+	if (dir == GPIO_DIR_OUT)
+		devmem_val |= (0x1 << gpio_desc->gpio.pin);
+
+	ret = devmem_write2((void *)gpio_desc->addr->dir_addr, 32, devmem_val);
+	if (ret < 0)
 		return -1;
-	ret = devmem_read(devmem_fd, 32, &devmem_val);
+
+	return 0;
+}
+
+static int _gpio_get_dir_reg(const gpio_desc_t *gpio_desc, uint8_t *dir)
+{
+	int ret;
+	uint32_t devmem_val;
+
+	if (!gpio_desc || !gpio_desc->addr)
+		return -1;
+
+	ret = devmem_read2((void *)gpio_desc->addr->dir_addr, 32, &devmem_val);
+	if (ret < 0)
+		return -1;
+
+	if ((devmem_val >> gpio_desc->gpio.pin) & 0x1)
+		*dir = GPIO_DIR_OUT;
+	else
+		*dir = GPIO_DIR_IN;
+
+	return 0;
+}
+
+
+static int _gpio_set_val_reg(gpio_desc_t *gpio_desc, uint8_t val)
+{
+	int ret;
+	uint32_t devmem_val;
+
+	if (!gpio_desc || !gpio_desc->addr)
+		return -1;
+
+	ret = devmem_read2((void *)gpio_desc->addr->out_addr, 32, &devmem_val);
 	if (ret < 0)
 		return -1;
 
@@ -268,41 +264,103 @@ int gpio_set(gpio_desc_t *gpio_desc, uint8_t val)
 	if (val)
 		devmem_val |= (0x1 << gpio_desc->gpio.pin);
 
-	ret = devmem_write(devmem_fd, 32, devmem_val);
-	if (ret < 0)
-		return -1;
-	ret = devmem_close(devmem_fd);
+	ret = devmem_write2((void *)gpio_desc->addr->out_addr, 32, devmem_val);
 	if (ret < 0)
 		return -1;
 
 	return 0;
 }
 
-int gpio_get(gpio_desc_t *gpio_desc, uint8_t *val)
+static int _gpio_get_val_reg(gpio_desc_t *gpio_desc, uint8_t *val)
 {
-	int devmem_fd, ret;
-	uint32_t devmem_val;
+	int ret;
 	void *gpio_addr;
+	uint32_t devmem_val;
+	uint8_t dir;
 
 	if (!gpio_desc || !gpio_desc->addr)
 		return -1;
 
-	if (gpio_desc->dir == GPIO_DIR_IN)
+	ret = _gpio_get_dir_reg(gpio_desc, &dir);
+	if (ret)
+		return -1;
+
+	if (dir == GPIO_DIR_IN)
 		gpio_addr = (void *)gpio_desc->addr->in_addr;
 	else
 		gpio_addr = (void *)gpio_desc->addr->out_addr;
-	devmem_fd = devmem_open(gpio_addr);
-	if (devmem_fd <= 0)
-		return -1;
 
-	ret = devmem_read(devmem_fd, 32, &devmem_val);
+	ret = devmem_read2(gpio_addr, 32, &devmem_val);
 	if (ret < 0)
 		return -1;
+
 	*val = (devmem_val >> gpio_desc->gpio.pin) & 0x1;
 
-	ret = devmem_close(devmem_fd);
-	if (ret < 0)
+	return 0;
+}
+
+int gpio_init(const char *gpioname, uint8_t gpiodir)
+{
+	gpio_desc_t gpio_desc;
+	int ret;
+
+	ret = _gpio_desc_init(&gpio_desc, gpioname);
+	if (ret) {
+		fprintf(stderr, "[ERROR] GPIO desc init failed.");
 		return -1;
+	}
+
+	ret = _gpio_set_pinmux_reg(&gpio_desc);
+	if (ret) {
+		fprintf(stderr, "[ERROR] GPIO Set pinmux reg failed.");
+		return ret;
+	}
+
+	ret = _gpio_set_dir_reg(&gpio_desc, gpiodir);
+	if (ret) {
+		fprintf(stderr, "[ERROR] GPIO Set dir reg failed.");
+		return ret;
+	}
+
+	return 0;
+}
+
+int gpio_set(const char *gpioname, uint8_t val)
+{
+	gpio_desc_t gpio_desc;
+	int ret;
+
+	ret = _gpio_desc_init(&gpio_desc, gpioname);
+	if (ret) {
+		fprintf(stderr, "[ERROR] GPIO desc init failed.");
+		return -1;
+	}
+
+	ret = _gpio_set_val_reg(&gpio_desc, val);
+	if (ret) {
+		fprintf(stderr, "[ERROR] GPIO set val failed.");
+		return -1;
+	}
+
+	return 0;
+}
+
+int gpio_get(const char *gpioname, uint8_t *val)
+{
+	gpio_desc_t gpio_desc;
+	int ret;
+
+	ret = _gpio_desc_init(&gpio_desc, gpioname);
+	if (ret) {
+		fprintf(stderr, "[ERROR] GPIO desc init failed.");
+		return -1;
+	}
+
+	ret = _gpio_get_val_reg(&gpio_desc, val);
+	if (ret) {
+		fprintf(stderr, "[ERROR] GPIO get val failed.");
+		return -1;
+	}
 
 	return 0;
 }
--- hardware/phytium_gpio/phytium_gpio.h
+++ hardware/phytium_gpio/phytium_gpio.h
@@ -3,48 +3,14 @@
 
 #include <stdint.h>
 
-/* Phytium 2004/D2000 */
-#define PINMUX_ADDR   0x28180000
-#define GPIO_0_ADDR   0x28004000
-#define GPIO_1_ADDR   0x28005000
-
-/* Common */
 #define GPIO_DIR_IN    0
 #define GPIO_DIR_OUT   1
 
-#define GPIO_CTRL_NUM  2
-#define GPIO_PORT_NUM  2
-#define GPIO_PIN_NUM   8
+#define GPIO_CTRL_NUM  2  /* 0, 1 */
+#define GPIO_PORT_NUM  2  /* a, b */
+#define GPIO_PIN_NUM   8  /* 0 ~ 7 */
 
 #define GPIO_NAME_LEN_MAX  8
-
-typedef struct gpio {
-	uint8_t ctrl;        /* gpio controller (0, 1) */
-	char    port;        /* gpio port (a, b) */
-	uint8_t pin;         /* gpio pin (0-7) */
-} gpio_t;
-
-typedef struct gpio_pinmux_cfg {
-	gpio_t   gpio;
-	uint64_t addr;       /* gpio pinmux address */
-	uint8_t  bit_offset; /* gpio pinmux offset (2 bits) */
-	uint8_t  val;        /* gpio pinmux value (2 bits) */
-} gpio_pinmux_cfg_t;
-
-typedef struct gpio_addr {
-	gpio_t   gpio;
-	uint64_t dir_addr;   /* gpio direction address */
-	uint64_t in_addr;    /* gpio input address */
-	uint64_t out_addr;   /* gpio output address */
-} gpio_addr_t;
-
-typedef struct gpio_desc {
-	gpio_t   gpio;
-	uint8_t  dir;        /* gpio direction */
-	uint8_t  val;        /* gpio value */
-	const gpio_pinmux_cfg_t *pinmux; /* gpio pinmux cfg info */
-	const gpio_addr_t       *addr;   /* gpio addr info */
-} gpio_desc_t;
 
 /**
  *  gpioname: gpio description string
@@ -53,10 +19,9 @@ typedef struct gpio_desc {
  *  gpiodir : gpio direction
  *            0 input, 1 output
  */
-gpio_desc_t *gpio_open(const char *gpioname, uint8_t gpiodir);
-int gpio_close(gpio_desc_t *gpio);
 
-int gpio_get(gpio_desc_t *gpio, uint8_t *val);
-int gpio_set(gpio_desc_t *gpio, uint8_t val);
+int gpio_init(const char *gpioname, uint8_t gpiodir);
+int gpio_set(const char *gpioname, uint8_t val);
+int gpio_get(const char *gpioname, uint8_t *val);
 
 #endif /* __PHYTIUM_GPIO_H__ */
