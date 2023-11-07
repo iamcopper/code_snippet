000ebec4f86f1c6b2a10eeb1693807c937b514ad
iamcopper <kangpan519@gmail.com>
2023-11-07 17:55:44 +0800

    [hardware/devmem] Re-write, remove status stored in devmem.c, return the (devmem_t *devmem) to user.

--- hardware/devmem/devmem.c
+++ hardware/devmem/devmem.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/mman.h>
 #include <sys/types.h>
@@ -10,38 +11,31 @@
 
 #define DEVMEM "/dev/mem"
 
-struct devmem {
-	void *addr;
-	void *virt_base_addr;
-	void *virt_addr;
-	size_t map_size;
-	int fd;
-};
-static struct devmem vm = {
-	.fd = -1,
-};
-
-int devmem_open(void *addr)
+devmem_t *devmem_open(void *addr)
 {
 	long page_size;
+	devmem_t *devmem = NULL;
 
 	if (addr == NULL)
-		return -1;
+		return NULL;
 
-	if (vm.fd != -1)
-		return -1;
-
-	vm.addr = addr;
-	vm.map_size = page_size = sysconf(_SC_PAGE_SIZE);
-	if (page_size <= 0) {
-		perror("sysconf failed");
-		return -1;
+	devmem = malloc(sizeof(devmem_t));
+	if (!devmem) {
+		perror("malloc devmem");
+		return NULL;
 	}
 
-	vm.fd = open(DEVMEM, O_SYNC | O_RDWR);
-	if (vm.fd < 0) {
+	devmem->addr = addr;
+	devmem->map_size = page_size = sysconf(_SC_PAGE_SIZE);
+	if (page_size <= 0) {
+		perror("sysconf failed");
+		return NULL;
+	}
+
+	devmem->fd = open(DEVMEM, O_SYNC | O_RDWR);
+	if (devmem->fd < 0) {
 		perror("open /dev/mem failed");
-		return -1;
+		return NULL;
 	}
 
 	/**
@@ -52,147 +46,140 @@ int devmem_open(void *addr)
 	 *  page_size-1    -> 0x0FFF
 	 *  ~(page_size-1) -> 0xF000
 	 */
-	vm.virt_base_addr = mmap(NULL,
-					vm.map_size,
+	devmem->vt_base_addr = mmap(NULL,
+					devmem->map_size,
 					PROT_READ | PROT_WRITE,
 					MAP_SHARED,
-					vm.fd,
-					(off_t)vm.addr & ~(page_size-1));
-	if (vm.virt_base_addr == MAP_FAILED)
+					devmem->fd,
+					(off_t)devmem->addr & ~(page_size-1));
+	if (devmem->vt_base_addr == MAP_FAILED)
 		goto err;
 
-	vm.virt_addr = vm.virt_base_addr + ((off_t)vm.addr & (page_size-1));
+	devmem->vt_addr = devmem->vt_base_addr + ((off_t)devmem->addr & (page_size-1));
 
-	return vm.fd;
+	return devmem;
 
 err:
-	if (vm.fd > 0) {
-		close(vm.fd);
-		vm.fd = -1;
-	}
+	if (devmem->fd > 0)
+		close(devmem->fd);
 
-	return -1;
+	return NULL;
 }
 
-int devmem_close(int fd)
+int devmem_close(devmem_t *devmem)
 {
 	int ret;
 
-	if (fd <= 0 || fd != vm.fd)
+	if (!devmem)
 		return -1;
 
-	ret = munmap(vm.virt_base_addr, vm.map_size);
+	ret = munmap(devmem->vt_base_addr, devmem->map_size);
 	if (ret) {
 		perror("munmap failed");
 		return -1;
 	}
 
-	ret = close(vm.fd);
+	ret = close(devmem->fd);
 	if (ret) {
 		perror("close /dev/mem failed");
 		return -1;
 	}
 
-	vm.fd = -1;
 	return 0;
 }
 
-int devmem_read(int fd, uint8_t offset, uint8_t width, uint32_t *val)
+int devmem_read(devmem_t *devmem, uint8_t offset, uint8_t width, uint32_t *val)
 {
-	int ret = 0;
-
-	if (fd <= 0 || fd != vm.fd)
+	if (!devmem || !val)
 		return -1;
 
 	switch (width) {
 		case 8:
-			*val = *((uint8_t *)vm.virt_addr + offset);
+			*val = *((uint8_t *)devmem->vt_addr + offset);
 			break;
 		case 16:
-			*val = *((uint16_t *)vm.virt_addr + offset);
+			*val = *((uint16_t *)devmem->vt_addr + offset);
 			break;
 		case 32:
-			*val = *(uint32_t *)(vm.virt_addr + offset);
+			*val = *(uint32_t *)(devmem->vt_addr + offset);
 			break;
 		default:
 			fprintf(stderr, "Read unsupport width: %d\n",width);
-			ret = -1;
+			return -1;
 			break;
 	}
 
-	return ret;
+	return 0;
 }
 
-int devmem_write(int fd, uint8_t offset, uint8_t width, uint32_t val)
+int devmem_write(devmem_t *devmem, uint8_t offset, uint8_t width, uint32_t val)
 {
-	int ret = 0;
-
-	if (fd <= 0 || fd != vm.fd)
+	if (!devmem)
 		return -1;
 
 	switch (width) {
 		case 8:
-			*(uint8_t *)(vm.virt_addr + offset) = val;
+			*(uint8_t *)(devmem->vt_addr + offset) = val;
 			break;
 		case 16:
-			*(uint16_t *)(vm.virt_addr + offset) = val;
+			*(uint16_t *)(devmem->vt_addr + offset) = val;
 			break;
 		case 32:
-			*(uint32_t *)(vm.virt_addr + offset) = val;
+			*(uint32_t *)(devmem->vt_addr + offset) = val;
 			break;
 		default:
 			fprintf(stderr, "Write unsupport width: %d\n", width);
-			ret = -1;
+			return -1;
 			break;
 	}
 
-	return ret;
+	return 0;
 }
 
-int devmem_clr_bit(int fd, uint8_t offset, uint8_t width, uint8_t bit)
+int devmem_clr_bit(devmem_t *devmem, uint8_t offset, uint8_t width, uint8_t bit)
 {
 	uint32_t val;
 
-	if (fd <= 0 || fd != vm.fd)
+	if (!devmem)
 		return -1;
 
-	if (0 > devmem_read(fd, offset, width, &val))
+	if (0 > devmem_read(devmem, offset, width, &val))
 		return -1;
 
 	val &= ~(1 << bit);
 
-	if (0 > devmem_write(fd, offset, width, val))
+	if (0 > devmem_write(devmem, offset, width, val))
 		return -1;
 
 	return 0;
 }
 
-int devmem_set_bit(int fd, uint8_t offset, uint8_t width, uint8_t bit)
+int devmem_set_bit(devmem_t *devmem, uint8_t offset, uint8_t width, uint8_t bit)
 {
 	uint32_t val;
 
-	if (fd <= 0 || fd != vm.fd)
+	if (!devmem)
 		return -1;
 
-	if (0 > devmem_read(fd, offset, width, &val))
+	if (0 > devmem_read(devmem, offset, width, &val))
 		return -1;
 
 	val |= (1 << bit);
 
-	if (0 > devmem_write(fd, offset, width, val))
+	if (0 > devmem_write(devmem, offset, width, val))
 		return -1;
 
 	return 0;
 }
 
-int devmem_get_bit(int fd, uint8_t offset, uint8_t width, uint8_t bit)
+int devmem_get_bit(devmem_t *devmem, uint8_t offset, uint8_t width, uint8_t bit)
 {
 	uint32_t val;
 
-	if (fd <= 0 || fd != vm.fd)
+	if (!devmem)
 		return -1;
 
-	if (0 > devmem_read(fd, offset, width, &val))
+	if (0 > devmem_read(devmem, offset, width, &val))
 		return -1;
 
 	return ((val >> bit) & 0x1);
@@ -200,153 +187,100 @@ int devmem_get_bit(int fd, uint8_t offset, uint8_t width, uint8_t bit)
 
 int devmem_read2(void *addr, uint8_t offset, uint8_t width, uint32_t *val)
 {
-	int fd, ret;
+	int ret;
+	devmem_t *devmem;
 
-	fd = devmem_open(addr);
-	if (fd <= 0)
+	devmem = devmem_open(addr);
+	if (!devmem)
 		return -1;
 
-	ret = devmem_read(fd, offset, width, val);
+	ret = devmem_read(devmem, offset, width, val);
 	if (ret < 0)
 		goto err;
 
-	return devmem_close(fd);
+	return devmem_close(devmem);
 
 err:
-	devmem_close(fd);
+	devmem_close(devmem);
 	return -2;
 }
 
 int devmem_write2(void *addr, uint8_t offset, uint8_t width, uint32_t val)
 {
-	int fd, ret;
+	int ret;
+	devmem_t *devmem;
 
-	fd = devmem_open(addr);
-	if (fd <= 0)
+	devmem = devmem_open(addr);
+	if (!devmem)
 		return -1;
 
-	ret = devmem_write(fd, offset, width, val);
+	ret = devmem_write(devmem, offset, width, val);
 	if (ret < 0)
 		goto err;
 
-	devmem_close(fd);
+	devmem_close(devmem);
 	return 0;
 
 err:
-	devmem_close(fd);
+	devmem_close(devmem);
 	return -2;
 }
 
 int devmem_clr_bit2(void *addr, uint8_t offset, uint8_t width, uint8_t bit)
 {
-	int fd;
+	devmem_t *devmem;
 
-	fd = devmem_open(addr);
-	if (fd <= 0)
+	devmem = devmem_open(addr);
+	if (!devmem)
 		return -1;
 
-	if (0 > devmem_clr_bit(fd, offset, width, bit))
+	if (0 > devmem_clr_bit(devmem, offset, width, bit))
 		goto err;
 
-	devmem_close(fd);
+	devmem_close(devmem);
 	return 0;
 
 err:
-	devmem_close(fd);
-	return -1;
+	devmem_close(devmem);
+	return -2;
 }
 
 int devmem_set_bit2(void *addr, uint8_t offset, uint8_t width, uint8_t bit)
 {
-	int fd;
+	devmem_t *devmem;
 
-	fd = devmem_open(addr);
-	if (fd <= 0)
+	devmem = devmem_open(addr);
+	if (!devmem)
 		return -1;
 
-	if (0 > devmem_set_bit(fd, offset, width, bit))
+	if (0 > devmem_set_bit(devmem, offset, width, bit))
 		goto err;
 
-	devmem_close(fd);
+	devmem_close(devmem);
 	return 0;
 
 err:
-	devmem_close(fd);
-	return -1;
+	devmem_close(devmem);
+	return -2;
 }
 
 int devmem_get_bit2(void *addr, uint8_t offset, uint8_t width, uint8_t bit)
 {
-	int fd;
+	devmem_t *devmem;
 	int val;
 
-	fd = devmem_open(addr);
-	if (fd <= 0)
+	devmem = devmem_open(addr);
+	if (!devmem)
 		return -1;
 
-	val = devmem_get_bit(fd, offset, width, bit);
+	val = devmem_get_bit(devmem, offset, width, bit);
 	if (val < 0)
 		goto err;
 
-	devmem_close(fd);
-	return val;
+	devmem_close(devmem);
+	return 0;
 
 err:
-	devmem_close(fd);
-	return -1;
-}
-
-int devmem_read_opt(int fd, void *addr, uint8_t offset, uint8_t width, uint32_t *val)
-{
-	if (vm.fd > 0)
-		return -1;
-
-	if (fd > 0 && vm.fd == fd)
-		return devmem_read(fd, offset, width, val);
-
-	return devmem_read2(addr, offset, width, val);
-}
-
-int devmem_write_opt(int fd, void *addr, uint8_t offset, uint8_t width, uint32_t val)
-{
-	if (vm.fd > 0)
-		return -1;
-
-	if (fd > 0 && vm.fd == fd)
-		return devmem_write(fd, offset, width, val);
-
-	return devmem_write2(addr, offset, width, val);
-}
-
-int devmem_clr_bit_opt(int fd, void *addr, uint8_t offset, uint8_t width, uint8_t bit)
-{
-	if (vm.fd > 0)
-		return -1;
-
-	if (fd > 0 && vm.fd == fd)
-		return devmem_clr_bit(fd, offset, width, bit);
-
-	return devmem_clr_bit2(addr, offset, width, bit);
-}
-
-int devmem_set_bit_opt(int fd, void *addr, uint8_t offset, uint8_t width, uint8_t bit)
-{
-	if (vm.fd > 0)
-		return -1;
-
-	if (fd > 0 && vm.fd == fd)
-		return devmem_set_bit(fd, offset, width, bit);
-
-	return devmem_set_bit2(addr, offset, width, bit);
-}
-
-int devmem_get_bit_opt(int fd, void *addr, uint8_t offset, uint8_t width, uint8_t bit)
-{
-	if (vm.fd > 0)
-		return -1;
-
-	if (fd > 0 && vm.fd == fd)
-		return devmem_get_bit(fd, offset, width, bit);
-
-	return devmem_get_bit2(addr, offset, width, bit);
+	devmem_close(devmem);
+	return -2;
 }
--- hardware/devmem/devmem.h
+++ hardware/devmem/devmem.h
@@ -3,24 +3,26 @@
 
 #include <stdint.h>
 
-int devmem_open(void *addr);
-int devmem_close(int fd);
-int devmem_read(int fd, uint8_t offset, uint8_t width, uint32_t *val);
-int devmem_write(int fd, uint8_t offset, uint8_t width, uint32_t val);
-int devmem_clr_bit(int fd, uint8_t offset, uint8_t width, uint8_t bit);
-int devmem_set_bit(int fd, uint8_t offset, uint8_t width, uint8_t bit);
-int devmem_get_bit(int fd, uint8_t offset, uint8_t width, uint8_t bit);
+typedef struct devmem {
+	void   *addr;
+	void   *vt_addr;
+	void   *vt_base_addr;
+	size_t map_size;
+	int    fd;
+} devmem_t;
+
+devmem_t *devmem_open(void *addr);
+int devmem_close(devmem_t *devmem);
+int devmem_read(devmem_t *devmem, uint8_t offset, uint8_t width, uint32_t *val);
+int devmem_write(devmem_t *devmem, uint8_t offset, uint8_t width, uint32_t val);
+int devmem_clr_bit(devmem_t *devmem, uint8_t offset, uint8_t width, uint8_t bit);
+int devmem_set_bit(devmem_t *devmem, uint8_t offset, uint8_t width, uint8_t bit);
+int devmem_get_bit(devmem_t *devmem, uint8_t offset, uint8_t width, uint8_t bit);
 
 int devmem_read2(void *addr, uint8_t offset, uint8_t width, uint32_t *val);
 int devmem_write2(void *addr, uint8_t offset, uint8_t width, uint32_t val);
 int devmem_clr_bit2(void *addr, uint8_t offset, uint8_t width, uint8_t bit);
 int devmem_set_bit2(void *addr, uint8_t offset, uint8_t width, uint8_t bit);
 int devmem_get_bit2(void *addr, uint8_t offset, uint8_t width, uint8_t bit);
-
-int devmem_read_opt(int fd, void *addr, uint8_t offset, uint8_t width, uint32_t *val);
-int devmem_write_opt(int fd, void *addr, uint8_t offset, uint8_t width, uint32_t val);
-int devmem_clr_bit_opt(int fd, void *addr, uint8_t offset, uint8_t width, uint8_t bit);
-int devmem_set_bit_opt(int fd, void *addr, uint8_t offset, uint8_t width, uint8_t bit);
-int devmem_get_bit_opt(int fd, void *addr, uint8_t offset, uint8_t width, uint8_t bit);
 
 #endif /* __DEVMEM_H__ */
