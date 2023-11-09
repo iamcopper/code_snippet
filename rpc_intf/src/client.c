c24d4b9d3c4dfde70645505745cc59850ce3d5db
iamcopper <kangpan519@gmail.com>
2023-11-09 13:03:10 +0800

    [makefile] uinfy almost all Makefiles again, fix .dep issues.

--- rpc_intf/src/client.c
+++ rpc_intf/src/client.c
@@ -20,8 +20,8 @@ int main(int argc, char *argv[])
 	rpc_addr_t servaddr;
 	rpc_msg_t msg;
 
-//	rpc_addr_pack(&servaddr, INADDR_LOOPBACK, RPC_SERVER_PORT);
-	rpc_addr_pack(&servaddr, sock_atoh("172.21.73.36"), RPC_SERVER_PORT);
+	rpc_addr_pack(&servaddr, INADDR_LOOPBACK, RPC_SERVER_PORT);
+//	rpc_addr_pack(&servaddr, sock_atoh("172.21.73.36"), RPC_SERVER_PORT);
 
 	intf = rpc_intf_open(OFLAG_CLIENT, &servaddr);
 
