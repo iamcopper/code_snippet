14b0fad8d2c8163015943e9da805d380e90e8a3e
iamcopper <kangpan519@gmail.com>
2023-11-15 18:36:48 +0800

    [kernel_module] Add hello_multi_mod example.

---
+++ kernel_module/hello_multi_mod/hello2.c
@@ -0,0 +1,24 @@
+#define DEBUG
+
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+
+static int hello_init(void)
+{
+	//printk(KERN_ALERT "Hello world\n");
+	//printk(KERN_ALERT "VAR1=%d\n", VAR1);
+	pr_debug("[DEBUG] init func=%s\n", __func__);
+	return 0;
+}
+
+static void hello_exit(void)
+{
+	//printk(KERN_ALERT "Goodbye world\n");
+	pr_debug("[DEBUG] exit func=%s\n", __func__);
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+MODULE_AUTHOR("iamcopper<kangpan519@gmail.com>");
+MODULE_LICENSE("GPL v2");
