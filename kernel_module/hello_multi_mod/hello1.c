4c344308ef2748ab17907f7c0844c278d662f313
iamcopper <kangpan519@gmail.com>
2023-12-21 16:34:42 +0800

    [kernel_module/hello_multi_mod] Add export symbole test in multi modules.

--- kernel_module/hello_multi_mod/hello1.c
+++ kernel_module/hello_multi_mod/hello1.c
@@ -4,18 +4,30 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 
+/* Export symbol to mod2 */
+static const char *hellostr = "Hello World.";
+static void echo_func(const char *str)
+{
+	pr_info("[%s-INFO] in func=%s: str=%s\n", module_name(THIS_MODULE), __func__, str);
+}
+EXPORT_SYMBOL(hellostr);
+EXPORT_SYMBOL(echo_func);
+
 static int hello_init(void)
 {
 	//printk(KERN_ALERT "Hello world\n");
 	//printk(KERN_ALERT "VAR1=%d\n", VAR1);
-	pr_debug("[DEBUG] init func=%s\n", __func__);
+	//pr_debug("[%s-DEBUG] init func=%s\n", module_name(THIS_MODULE), __func__);
+	pr_info("[%s-INFO] in init=%s\n", THIS_MODULE->name, __func__);
+	pr_info("[%s-INFO] hellostr=%s\n", THIS_MODULE->name, hellostr);
+	echo_func(hellostr);
 	return 0;
 }
 
 static void hello_exit(void)
 {
 	//printk(KERN_ALERT "Goodbye world\n");
-	pr_debug("[DEBUG] exit func=%s\n", __func__);
+	pr_debug("[%s-DEBUG] in exit=%s\n", THIS_MODULE->name, __func__);
 }
 
 module_init(hello_init);
--- kernel_module/hello_multi_mod/hello2.c
+++ kernel_module/hello_multi_mod/hello2.c
@@ -4,18 +4,26 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 
+/* Access symbols exported from mod1 */
+extern const char *hellostr;
+extern void echo_func(const char *str);
+
 static int hello_init(void)
 {
 	//printk(KERN_ALERT "Hello world\n");
 	//printk(KERN_ALERT "VAR1=%d\n", VAR1);
-	pr_debug("[DEBUG] init func=%s\n", __func__);
+	//pr_debug("[%s-DEBUG] init func=%s\n", module_name(THIS_MODULE), __func__);
+	pr_info("[%s-INFO] in init=%s\n", THIS_MODULE->name, __func__);
+	pr_info("[%s-INFO] hellostr=%s\n", THIS_MODULE->name, hellostr);
+	echo_func(hellostr);
+
 	return 0;
 }
 
 static void hello_exit(void)
 {
 	//printk(KERN_ALERT "Goodbye world\n");
-	pr_debug("[DEBUG] exit func=%s\n", __func__);
+	pr_debug("[%s-DEBUG] in exit=%s\n", THIS_MODULE->name, __func__);
 }
 
 module_init(hello_init);
