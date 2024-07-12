cfc37132c3767798430209b4bd43ce8a5d06ee1b
iamcopper <kangpan519@gmail.com>
2024-07-12 11:35:19 +0800

    [test_script] Add retry mechanism for auto_reset.sh

--- test_script/auto_reset.sh
+++ test_script/auto_reset.sh
@@ -4,7 +4,7 @@ echo -e "*********************************"
 echo -e "*          Auto Reset Test      *"
 echo -e "*********************************"
 
-HOST="192.168.1.157"
+HOST="192.168.2.53"
 USR="root"
 PWD="123"
 SLEEP="60"
@@ -12,6 +12,7 @@ SSH_CMD="reboot"
 
 LOOP=$1
 TIMEOUT=3
+RETRY=5
 
 echo -e "HOST=${HOST}"
 echo -e "CMD=${CMD}"
@@ -20,9 +21,17 @@ echo -e "LOOP=${LOOP}\n"
 for (( i = 0; i < ${LOOP}; i++ )); do
 	echo -e "========== FOR LOOP $i =========="
 
+	let retry=1
 	CMD="timeout ${TIMEOUT} sshpass -p ${PWD} ssh -l ${USR} ${HOST} ${SSH_CMD}"
 	echo -e "${CMD}"
 	${CMD}
+	while [[ $? == 124 ]] && [[ ${retry} -le ${RETRY} ]];
+	do
+		sleep 3
+		echo -e "[INFO] ssh timeout, sleep 3s and retry=${retry}/${RETRY} ...\n"
+		let retry=${retry}+1
+		${CMD}
+	done
 	if [[ $? == 124 ]]; then
 		echo -e "[ERROR] ssh timeout, exit, i=${i}\n"
 		exit 1
