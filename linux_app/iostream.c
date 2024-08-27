a2c7590218ae8513c0a8110df42471111d790f05
iamcopper <kangpan519@gmail.com>
2024-08-27 17:40:19 +0800

    [linux_app] Add iostream.c

---
+++ linux_app/iostream.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <error.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	int ret = 0;
+
+	const char *str1 = "stdout stream: aaaaaaa\n";
+	const char *str2 = "stderr stream: bbbbbbb\n";
+	ret = write(STDOUT_FILENO, str1, strlen(str1));
+	//if (ret < 0)
+	//	perror("write1");
+	//else
+	//	printf("ret1=%d\n", ret);
+
+	ret = write(STDERR_FILENO, str2, strlen(str2));
+	//if (ret < 0)
+	//	perror("write2");
+	//else
+	//	printf("ret2=%d\n", ret);
+
+	return 0;
+}
