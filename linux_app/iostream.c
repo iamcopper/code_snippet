23bcae1572e51c3fae1855a4a80acfa4093ddd10
iamcopper <kangpan519@gmail.com>
2025-04-27 16:49:02 +0800

    [linux_app] Update iostream.c

--- linux_app/iostream.c
+++ linux_app/iostream.c
@@ -10,16 +10,16 @@ int main(int argc, char *argv[])
 	const char *str1 = "stdout stream: aaaaaaa\n";
 	const char *str2 = "stderr stream: bbbbbbb\n";
 	ret = write(STDOUT_FILENO, str1, strlen(str1));
-	//if (ret < 0)
-	//	perror("write1");
-	//else
-	//	printf("ret1=%d\n", ret);
+	if (ret < 0)
+		perror("write1");
+	else
+		printf("ret1=%d\n", ret);
 
 	ret = write(STDERR_FILENO, str2, strlen(str2));
-	//if (ret < 0)
-	//	perror("write2");
-	//else
-	//	printf("ret2=%d\n", ret);
+	if (ret < 0)
+		perror("write2");
+	else
+		printf("ret2=%d\n", ret);
 
 	return 0;
 }
