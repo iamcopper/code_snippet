b1f07a2eefe2172603bb0021d67ce6ed91553e61
iamcopper <kangpan519@gmail.com>
2025-12-16 13:32:34 +0800

    [test_script/auto_reset.sh] Add output message "ret=0" for ssh command.

--- test_script/auto_reset.sh
+++ test_script/auto_reset.sh
@@ -4,15 +4,15 @@ echo -e "*********************************"
 echo -e "*          Auto Reset Test      *"
 echo -e "*********************************"
 
-HOST="192.168.3.20"
+HOST="192.168.100.101"
 USR="root"
 PWD="123"
-SLEEP="120"
+SLEEP="60"
 SSH_CMD="reboot"
 
 LOOP=${1:-9999999999}
 TIMEOUT=3
-RETRY=5
+RETRY=3
 
 echo -e "HOST=${HOST}"
 echo -e "SSH_CMD=${SSH_CMD}"
@@ -25,15 +25,24 @@ for (( i = 0; i < ${LOOP}; i++ )); do
 	CMD="timeout ${TIMEOUT} sshpass -p ${PWD} ssh -l ${USR} ${HOST} ${SSH_CMD}"
 	echo -e "${CMD}"
 	${CMD}
-	while [[ $? == 124 ]] && [[ ${retry} -le ${RETRY} ]];
+	ret=$?
+	while [[ ${ret} -eq 124 ]] && [[ ${retry} -le ${RETRY} ]];
 	do
-		sleep 3
-		echo -e "[INFO] ssh timeout, sleep 3s and retry=${retry}/${RETRY} ...\n"
+		sleep 1
+		echo -e "[INFO] ssh timeout, sleep 1s and retry=${retry}/${RETRY} ..."
 		let retry=${retry}+1
 		${CMD}
+		ret=$?
 	done
-	if [[ $? == 124 ]]; then
+	echo "ret=${ret}"
+	# ret=124 --- ssh timeout
+	if [[ ${ret} -eq 124 ]]; then
 		echo -e "[ERROR] ssh timeout, exit, i=${i}\n"
+		exit 1
+	# ret=0   --- command narmally exit
+	# ret=255 --- close by remote
+	elif [[ ${ret} -ne 0 ]] && [[ ${ret} -ne 255 ]]; then
+		echo -e "[ERROR] ssh command exec error, exit, i=${i}\n"
 		exit 1
 	fi
 	echo -e "Waiting ${SLEEP} seconds ... \n"
